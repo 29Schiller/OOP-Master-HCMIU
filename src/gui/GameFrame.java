9ed1a8f33b5de9f1fa07a1214659a3dbdc2eff5d
Dat2906041995 <121652492+29Schiller@users.noreply.github.com>
2024-05-20 11:23:32 +0700

    Copy src code

---
+++ src/gui/GameFrame.java
@@ -0,0 +1,43 @@
+package Screnes;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+import Graphics.GamePanel;
+
+public class GameFrame extends JFrame {
+    public static final int SCREEN_WIDTH = 1300;
+    public static final int SCREEN_HEIGHT = 750;
+    GamePanel gamePanel;
+    GameLoop gameLoop;
+
+    public GameFrame() {
+        Toolkit toolkit = this.getToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        this.setBounds((dimension.width - SCREEN_WIDTH) / 2, (dimension.height - SCREEN_HEIGHT) / 2, SCREEN_WIDTH, SCREEN_HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gamePanel = new GamePanel();
+        gameLoop = new GameLoop();
+        add(gamePanel);
+    }
+
+    public static int getScreenWidth() {
+        return SCREEN_WIDTH;
+    }
+
+    public static int getScreenHeight() {
+        return SCREEN_HEIGHT;
+    }
+
+    public void startGame() {
+        Thread gameThread = new Thread(gameLoop);
+        gameThread.start();
+    }
+
+    public static void main(String[] args) {
+        GameFrame gameFrame = new GameFrame();
+        gameFrame.setVisible(true);
+        gameFrame.startGame();
+    }
+}
---
+++ src/gui/GamePanel.java
@@ -0,0 +1,19 @@
+package Graphics;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class GamePanel extends JPanel {
+    Background bg = new Background(this);
+
+    public GamePanel() {
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        bg.render(g2);
+    }
+}
